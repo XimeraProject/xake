a8532b0159119a0dbdd70cf31c7651938f01bb20
Jim Fowler <fowler@math.osu.edu>
2017-05-20 10:15:57 -0400

    Bumping version to v0.4.1

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 
 	app.Name = "xake"
 	app.Usage = "a build tool (make) for Ximera"
-	app.Version = "0.4.0"
+	app.Version = "0.4.1"
 
 	// Check to see if this is the newest version
 	go func() {
